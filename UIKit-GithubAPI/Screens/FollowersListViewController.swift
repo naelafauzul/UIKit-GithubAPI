f1cb20bcc7bf666bbfb7481715a57a99bf268ff9
Naela Fauzul Muna <naelafauzulm@gmail.com>
2026-01-31 15:22:15 +0700

    feat: handling networking

--- UIKit-GithubAPI/Screens/FollowersListViewController.swift
+++ UIKit-GithubAPI/Screens/FollowersListViewController.swift
@@ -10,11 +10,26 @@ import UIKit
 class FollowersListViewController: UIViewController {
     
     var username: String!
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .systemBackground
-        navigationController?.isNavigationBarHidden = false
         navigationController?.navigationBar.prefersLargeTitles = true
+        
+        NetworkManager.shared.getFollowers(for: username, page: 1) { result in
+            
+            switch result {
+            case .success(let followers):
+                print(followers)
+            case .failure(let error):
+                self.presentGFAlertOnMainThread(title: "Bad Stuff Happened", message: error.rawValue, buttonTitle: "OK")
+                
+            }
+        }
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        navigationController?.setNavigationBarHidden(false, animated: true)
     }
 }

ae963df1529e05800958cec4f57d35c89a0fffb0
Naela Fauzul Muna <naelafauzulm@gmail.com>
2026-02-01 14:11:17 +0700

    feat: pagination

--- UIKit-GithubAPI/Screens/FollowersListViewController.swift
+++ UIKit-GithubAPI/Screens/FollowersListViewController.swift
@@ -13,16 +13,20 @@ class FollowersListViewController: UIViewController {
     
     var username: String!
     var followers: [Follower] = []
+    var page = 1
+    var hasMoreFollowers = true
+    var isLoadingMoreFollowers = false
+
     
     var collectionView: UICollectionView!
     var dataSource: UICollectionViewDiffableDataSource<Section, Follower>!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        configureCollectionView()
         configureViewController()
-        getFollowers()
+        configureCollectionView()
         configureDataSource()
+        getFollowers(username: username, page: page)
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -38,22 +42,24 @@ class FollowersListViewController: UIViewController {
     func configureCollectionView(){
         collectionView = UICollectionView(frame: view.bounds, collectionViewLayout: UIHelper.createThreeColumnFlowLayout(in: view))
         view.addSubview(collectionView)
+        collectionView.delegate = self
         collectionView.backgroundColor = .systemBackground
         collectionView.register(FollowerCell.self, forCellWithReuseIdentifier: FollowerCell.reuseID)
     }
     
-    func getFollowers(){
-        NetworkManager.shared.getFollowers(for: username, page: 1) { [weak self] result in
+    func getFollowers(username: String, page: Int) {
+        NetworkManager.shared.getFollowers(for: username, page: page) { [weak self] result in
             guard let self = self else { return }
-            
+            self.isLoadingMoreFollowers = false   // penting
+
             switch result {
-            case .success(let followers):
-                self.followers = followers
-                self.updateData(with: followers)
-                
+            case .success(let newFollowers):
+                if newFollowers.count < 100 { self.hasMoreFollowers = false }
+                self.followers.append(contentsOf: newFollowers)
+                self.updateData()
+
             case .failure(let error):
                 self.presentGFAlertOnMainThread(title: "Bad Stuff Happened", message: error.rawValue, buttonTitle: "OK")
-                
             }
         }
     }
@@ -66,13 +72,28 @@ class FollowersListViewController: UIViewController {
         })
     }
     
-    func updateData(with followers: [Follower]) {
+    func updateData() {
         var snapshot = NSDiffableDataSourceSnapshot<Section, Follower>()
         snapshot.appendSections([.main])
-        snapshot.appendItems(followers)
+        snapshot.appendItems(self.followers)   // penting: pakai semua data
         DispatchQueue.main.async {
             self.dataSource.apply(snapshot, animatingDifferences: true)
         }
     }
+}
 
+extension FollowersListViewController: UICollectionViewDelegate {
+    
+    func scrollViewDidEndDragging(_ scrollView: UIScrollView, willDecelerate decelerate: Bool) {
+        let offsetY = scrollView.contentOffset.y
+        let contentHeight = scrollView.contentSize.height
+        let height = scrollView.frame.size.height
+        
+        if offsetY > contentHeight - height {
+            guard hasMoreFollowers, !isLoadingMoreFollowers else { return }
+            isLoadingMoreFollowers = true
+            page += 1
+            getFollowers(username: username, page: page)
+        }
+    }
 }
